fcb0feb33d37cf75539551a07e56d1ac452bcdc2
DevCapta1n <jackwinford@gmail.com>
2023-11-02 21:53:06 -0800

    fighting art and full screen

--- main.cpp
+++ main.cpp
@@ -299,7 +299,7 @@ void RenderTileMap(SDL_Renderer* renderer, SDL_Texture* tileset, int tileWidth, 
 }
 
 void render() {
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
 
     if (testTile != nullptr) {
@@ -340,14 +340,15 @@ int main(int argc, char* argv[]) {
     }
 
     // Create a window
-    SDL_Window* window = SDL_CreateWindow("hexagonal tesselation",
-        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, winInitWidth, winInitHeight, 0);
+    SDL_Window* window = SDL_CreateWindow("local man forced to fight robots on a hex grid",
+        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, 0);
 
     if (!window) {
         SDL_Log("Window could not be created! SDL_Error: %s\n", SDL_GetError());
         return 1;
     }
 
+    SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
     // initialize the renderer variable (already declared globally)
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
